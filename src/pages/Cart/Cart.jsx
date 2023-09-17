8762dd4d86bd212d054d6cc2f9d3928b7d28f1df
Quanghiep318 <135422019+Quanghiep318@users.noreply.github.com>
2023-09-17 22:45:47 +0900

    up

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -12,35 +12,35 @@ function Cart() {
   return (
     <div>
         {/* cart */}
-<div className="cart-section mt-150 mb-150">
-  <div className="container">
-    <div className="row">
-      <div className="col-lg-8 col-md-12">
-        <div className="cart-table-wrap">
-          <table className="cart-table">
-            <thead className="cart-table-head">
-              <tr className="table-head-row">
-                <th className="product-remove" />
-                <th className="product-image">Product Image</th>
-                <th className="product-name">Name</th>
-                <th className="product-price">Price</th>
-                <th className="product-quantity">Quantity</th>
-                <th className="product-total">Total</th>
+<div>
+  <div>
+    <div>
+      <div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th/>
+                <th>Product Image</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
               </tr>
             </thead>
             <tbody>
-              <tr className="table-body-row">
-                <td className="product-remove"><a href="#"><i className="far fa-window-close" /></a></td>
-                <td className="product-image"><img src="assets/img/products/product-img-1.jpg" alt /></td>
-                <td className="product-name">Strawberry</td>
-                <td className="product-price">$85</td>
+              <tr>
+                <td><a href="#"><i className="far fa-window-close" /></a></td>
+                <td><img src="assets/img/products/product-img-1.jpg" alt /></td>
+                <td>SP</td>
+                <td>$85</td>
                 <td className="product-quantity"><input type="number" placeholder={0} /></td>
                 <td className="product-total">1</td>
               </tr>
               <tr className="table-body-row">
                 <td className="product-remove"><a href="#"><i className="far fa-window-close" /></a></td>
                 <td className="product-image"><img src="assets/img/products/product-img-2.jpg" alt /></td>
-                <td className="product-name">Berry</td>
+                <td className="product-name">SP</td>
                 <td className="product-price">$70</td>
                 <td className="product-quantity"><input type="number" placeholder={0} /></td>
                 <td className="product-total">1</td>
@@ -48,7 +48,7 @@ function Cart() {
               <tr className="table-body-row">
                 <td className="product-remove"><a href="#"><i className="far fa-window-close" /></a></td>
                 <td className="product-image"><img src="assets/img/products/product-img-3.jpg" alt /></td>
-                <td className="product-name">Lemon</td>
+                <td className="product-name">SP!</td>
                 <td className="product-price">$35</td>
                 <td className="product-quantity"><input type="number" placeholder={0} /></td>
                 <td className="product-total">1</td>
@@ -89,15 +89,7 @@ function Cart() {
             {/* <a href="checkout.html" className="boxed-btn black">Check Out</a> */}
           </div>
         </div>
-        <div className="coupon-section">
-          <h3>Apply Coupon</h3>
-          <div className="coupon-form-wrap">
-            <form action="index.html">
-              <p><input type="text" placeholder="Coupon" /></p>
-              <p><input type="submit" defaultValue="Apply" /></p>
-            </form>
-          </div>
-        </div>
+        
       </div>
     </div>
   </div>
---
+++ src/redux/reducer/reducer.js
@@ -0,0 +1,24 @@
+import * as typeAction from "../constant/typeAction";
+// Khởi tạo init State
+const initState = {
+  products: [],
+  selectedProduct: null,
+};
+
+// Khởi tạo hàm xử lý logic
+const productReducer = (state = initState, action) => {
+    switch (action.type) {
+      case typeAction.ADD_PRODUCT:
+        return {
+          ...state,
+          products: [...state.products, action.payload],
+        };
+      
+  
+      default:
+        break;
+    }
+    return state;
+  };
+  
+  export default productReducer;
